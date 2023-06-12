5ca657c67c60b24698bc5031e79a3682b0943b6b
JonathanAristides <Jonathan.nuel98@gmail.com>
2023-06-12 14:18:52 +0200

    implements basic darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import {Button} from "@chakra-ui/react";
-
+import React from "react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { NavBar } from "./components/NavBar.tsx";
 
 function App() {
-    return (
-        <Button colorScheme="blue">Button</Button>
-    );
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+      <GridItem area="main" bg={"lightblue"}>
+        Main
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg={"yellow"}>
+          Aside
+        </GridItem>
+      </Show>
+    </Grid>
+  );
 }
 
 export default App;
